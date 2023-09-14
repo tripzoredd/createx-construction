5a760bc51ede9f7a755b075f6bc200bdd08a4560
tripzoredd <0997084@gmail.com>
2023-09-14 16:42:14 +0300

    исправление ошибок

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-
+$(window).on('load scroll', function() { 
+    if ($(this).scrollTop() >= '1') {
+        $('.header').css('background', 'orange');
+    } else {
+        $('.header').css('background', 'none');
+    }
+});
